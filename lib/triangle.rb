9ed70bd6621356c346141b6de33aeb4c0cf1afad
Matthew Phillips <phillips.matthew829@gmail.com>
2022-04-01 16:44:56 -0500

    commit tree 87739f21f25769941eda5e5970fdfa94fb5a6ebd

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    raise TriangleError unless [a, b, c].all? {|s| s > 0} && a + b > c && b + c > a && a + c > b
+
+    if a == b && a == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
